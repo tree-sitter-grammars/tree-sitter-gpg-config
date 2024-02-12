e8a95ae3deed7d7637c33f635fc5b9fc9dbb3216
ObserverOfTime <chronobserver@disroot.org>
2024-02-12 17:29:18 +0200

    chore: update everything

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # tree-sitter-gpg-config
 
-[![CI][badge]](https://github.com/ObserverOfTime/tree-sitter-gpg-config/actions)
+[![CI][ci]](https://github.com/tree-sitter-grammars/tree-sitter-gpg-config/actions/workflows/ci.yml)
+[![discord][discord]](https://discord.gg/w7nTvsVJhm)
+[![matrix][matrix]](https://matrix.to/#/#nvim-treesitter:matrix.org)
 
 A tree-sitter parser for GPG configuration files.
 
@@ -22,5 +24,8 @@ A tree-sitter parser for GPG configuration files.
 - [x] Neovim
 - [ ] Helix
 - [ ] Emacs
+- [ ] Zed
 
-[badge]: https://badgen.net/github/checks/ObserverOfTime/tree-sitter-gpg-config?label=CI&icon=github
+[ci]: https://img.shields.io/github/actions/workflow/status/tree-sitter-grammars/tree-sitter-gpg-config/ci.yml?logo=github&label=CI
+[discord]: https://img.shields.io/discord/1063097320771698699?logo=discord&label=tree-sitter
+[matrix]: https://img.shields.io/matrix/nvim-treesitter%3Amatrix.org?logo=matrix&label=nvim-treesitter
--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -4,10 +4,18 @@
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = "";
+//! let code = r#"
+//! no-greeting
+//! no-permission-warning
+//! lock-never
+//! keyserver-options timeout=10
+//! keyserver-options import-clean
+//! keyserver-options no-self-sigs-only
+//! "#;
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_gpg::language()).expect("Error loading gpg grammar");
+//! parser.set_language(tree_sitter_gpg_config::language()).expect("Error loading gpg grammar");
 //! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -31,14 +39,7 @@ pub fn language() -> Language {
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
